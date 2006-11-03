3c559e84487a63b52c0f0256dc18bde5f1b49709
shino <shino@8c9cce2d-531c-0410-91e8-f72d6ccfd7b9>
2006-11-03 16:43:12 +0000

    Forgot to commit 2 new classes : sets and maps.
    git-svn-id: http://svn.gna.org/svn/nevertable/trunk@22 8c9cce2d-531c-0410-91e8-f72d6ccfd7b9

---
+++ classes/class.map.php
@@ -0,0 +1,153 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+# This file is part of Nevertable .
+# Copyright (c) 2004 Francois Guillet and contributors. All rights
+# reserved.
+#
+# Nevertable is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Nevertable is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Nevertable; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# ***** END LICENSE BLOCK *****
+
+class Map
+{
+    var $db;
+    var $fields;
+    var $isload;
+
+	/*__Constructeur__
+	Cette fonction initialise l'objet Map.
+
+    @param: pointeur vers la base de donnée
+	*/
+	function Map(&$db)
+	{
+        $this->db = &$db;
+        $this->isload = false;
+	}
+
+    /* Chargement des champs d'un record à partir de l'id */
+    function LoadFromId($id)
+    {
+      $this->isload = false;
+      unset($this->fields);
+      if (empty($id))
+        return false;
+      $this->db->RequestInit("SELECT", "maps");
+      $this->db->RequestGenericFilter("id", $id);
+      if(!$this->db->Query()) {
+        $this->SetError($this->db->GetError());
+        return false;
+      }
+      else {
+        if ($this->db->NumRows()<1)
+        {
+          $this->error = "No map match this id!";
+          return false;
+        }
+        $this->SetFields($this->db->FetchArray());
+        $this->isload=true;
+        return true;
+      }
+    }
+
+    function Update($conservative=false,$id="")
+    {
+      if (!$this->isload)
+        return false;
+      if(empty($id))
+        $id = $this->fields['id'];
+
+      $this->_CleanFields();
+      $this->db->RequestInit("UPDATE", "maps");
+      $this->db->RequestUpdateSet($this->fields, $conservative);
+      $this->db->RequestGenericFilter("id", $id);
+      $this->db->RequestLimit(1);
+      if(!$this->db->Query()) {
+        $this->SetError($this->db->GetError());
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+
+    function Purge($id="")
+    {
+      if (!$this->isload)
+        return false;
+      if(empty($id))
+        $id = $this->fields['id'];
+      
+      return false;
+
+    }
+
+    function Insert()
+    { 
+      if (!$this->isload)
+        return false;
+      return false;
+    }
+
+    function GetId()
+    {
+      return $this->fields['id'];
+    }
+    
+    function GetSetId()
+    {
+      return $this->fields['set_id'];
+    }
+    
+    function GetLevelNum()
+    {
+      return $this->fields['level_num'];
+    }
+    
+    function GetLevelName()
+    {
+      return $this->fields['level_name'];
+    }
+    
+    function GetSolFile()
+    {
+      return $this->fields['map_solfile'];
+    }
+    
+    function SetFields($fields)
+    {
+        foreach ($fields as $name => $value)
+        {
+            if (!empty($name) && (
+                $name == "id" 
+             || $name == "set_id"
+             || $name == "level_num"
+             || $name == "level_name"
+             || $name == "map_solfile"))
+                   $this->fields[$name] = $value;
+        }
+        $this->isload=true;
+    }
+    
+    function SetError($error)
+    {
+      $this->error = $error;
+    }
+    
+    function GetError()
+    {
+      return $this->error;
+    }
+}
